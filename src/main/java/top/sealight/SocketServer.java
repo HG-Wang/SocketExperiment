60b38659471a0eca3da2be41446b05c5c825c38f
HG_Wang <cnhgwang@gmail.com>
2024-12-31 17:28:29 +0800

    main方法,配置测试服务器

--- src/main/java/top/sealight/SocketServer.java
+++ src/main/java/top/sealight/SocketServer.java
@@ -1,10 +1,11 @@
 package top.sealight;
 
 
-import java.io.IOException;
+import java.io.*;
 import java.net.ServerSocket;
 
 import java.net.Socket;
+import java.nio.charset.StandardCharsets;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -51,6 +52,43 @@ public class SocketServer {
 
         @Override
         public void run() {
+            //try-with-resources
+            try(
+                //获取输入流,用以接收客户端消息
+                BufferedReader reader = new BufferedReader(new InputStreamReader(
+                        socket.getInputStream(), StandardCharsets.UTF_8));
+                //获取输出流,用以向客户端发送消息
+                BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(
+                        socket.getOutputStream(),StandardCharsets.UTF_8));
+            ) {
+                String line;
+                //接收并打印客户端消息
+                while((line = reader.readLine())!= null){
+                    System.out.println("客户端< "+socket.getRemoteSocketAddress() +"> 消息" +line);
+
+                    //将消息回显给客户端,实现双工通信
+                    writer.write("服务器已收到: "+line);
+                    writer.newLine();
+                    writer.flush();
+                }
+            }catch(IOException e) {
+                System.err.println("客户端通信异常: "+e.getMessage());
+            }finally {
+                try{
+                    socket.close();
+                } catch (IOException e) {
+                    System.err.println("关闭客户端Socket时出现异常: "+e.getMessage());
+                }
+                System.out.println("客户端< "+socket.getRemoteSocketAddress() +"> 已断开连接");
+            }
+        }
+
+        public static void main(String[] args) {
+            int port = 12345;  //服务器监听端口
+            int maxClient = 10;  //最大客户端连接个数
+            //启动服务器
+            SocketServer server = new SocketServer(port,maxClient);
+            server.start();
 
         }
     }
