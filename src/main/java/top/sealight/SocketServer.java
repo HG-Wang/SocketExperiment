4fa5bfbe73f62c634df40ee5f1bfa5f608229539
HG_Wang <cnhgwang@gmail.com>
2024-12-31 14:42:13 +0800

    start方法,ClientHandler 框架

--- src/main/java/top/sealight/SocketServer.java
+++ src/main/java/top/sealight/SocketServer.java
@@ -4,6 +4,7 @@ package top.sealight;
 import java.io.IOException;
 import java.net.ServerSocket;
 
+import java.net.Socket;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -23,6 +24,34 @@ public class SocketServer {
             System.err.println("无法绑定到端口 "+port+",请检查端口是否被占用");
             //出错后退出程序
             System.exit(-1);
+        }
+    }
+
+    public void start(){
+        while (true){
+            try{
+                //等待客户端连接
+                Socket clientSocket = serverSocket.accept();
+                System.out.println("有客户端连接: " + clientSocket.getRemoteSocketAddress());
+                //将客户端的处理逻辑交给线程池
+                threadPoll.execute(new ClientHandler(clientSocket));
+            } catch (IOException e) {
+                System.err.println("接收客户端连接时出现差错");
+            }
+        }
+    }
+
+    //用以处理客户端消息
+    private static class ClientHandler implements Runnable{
+        private Socket socket;
+
+        public ClientHandler(Socket socket){
+            this.socket = socket;
+        }
+
+        @Override
+        public void run() {
+
         }
     }
 
