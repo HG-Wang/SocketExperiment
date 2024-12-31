161393b5cff428eafd4befe6e0280d3aeea9b480
HG_Wang <cnhgwang@gmail.com>
2024-12-31 10:16:50 +0800

    初始化项目+服务端基本成员变量

---
+++ src/main/java/top/sealight/SocketServer.java
@@ -0,0 +1,15 @@
+package top.sealight;
+
+
+import java.net.ServerSocket;
+
+import java.util.concurrent.ExecutorService;
+
+
+public class SocketServer {
+    private ServerSocket serverSocket;
+    private ExecutorService threadPoll;
+
+
+}
+
